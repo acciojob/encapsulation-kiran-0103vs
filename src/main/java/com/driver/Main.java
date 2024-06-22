777e4fb5fe00764df950135d47f3feecd00113bc
kiran-0103vs <kiranvenkatesan08@gmail.com>
2024-06-22 14:34:52 +0530

    emcapsulation done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        RWOnly obj = new RWOnly();
+        //obj.name = "kiran"; // This will cause an error
+
+        // Trying to print the value of name
+        // System.out.println(obj.name);
+        //Error
+        obj.setName("kiran");
+
+
+        System.out.println(obj.getName());
+    }
   
 }
